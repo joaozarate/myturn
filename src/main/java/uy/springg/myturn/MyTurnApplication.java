ed5c7aad20d094fe4c02a881c0abec79a08676cf
Joao Zarate <joaozarate@gmail.com>
2023-06-10 16:09:54 +0100

    Adds initial project structure.

---
+++ src/main/java/uy/springg/myturn/MyTurnApplication.java
@@ -0,0 +1,13 @@
+package uy.springg.myturn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTurnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTurnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uy/springg/myturn/MyTurnApplicationTests.java
@@ -0,0 +1,13 @@
+package uy.springg.myturn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTurnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
